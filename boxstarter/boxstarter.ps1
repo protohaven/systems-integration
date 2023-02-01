6063bf2100a2f9cfe8a82954716caa4c00150e30
Jeff Bearer <jebearer@microsoft.com>
2023-02-01 09:22:10 -0500

    added download for prusa slicer 2.6 alpha2.

--- boxstarter/boxstarter.ps1
+++ boxstarter/boxstarter.ps1
@@ -181,6 +181,7 @@ $ManualDownloadInstall = @{
    'Amana-Tool-Vectric.tool' = 'https://www.amanatool.com/pub/media/vectriclibrary/Amana-Tool-Vectric.tool'
    'Whiteside-Vectric-File-Library.tool' = 'https://www.dropbox.com/sh/3n5m5mxybdl98da/AAB9Qy_Z-c6z5w_F1Kcuopaga?dl=1&preview=Whiteside+Vectric+File+Library.tool'
    'BitsBits-Vectric-Tool-Library.V1.3.5.zip' = 'https://bitsbits.com/wp-content/uploads/2022/03/BitsBits-Vectric-Tool-Library.V1.3.5.zip'
+   'PrusaSlicer-2.6.0-alpha2.zip' = 'https://github.com/prusa3d/PrusaSlicer/releases/download/version_2.6.0-alpha2/PrusaSlicer-2.6.0-alpha2+win64-202301311700_signed.zip'
 }
 
 $WebShortCuts = @{
